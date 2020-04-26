7e707e3c75986d0ec76704a3244ec377542b188f
mwilkens1 <mathijn.wilkens@gmail.com>
2020-04-26 18:39:43 +0100

    githublink

--- app_web/app.R
+++ app_web/app.R
@@ -239,9 +239,17 @@ ui <- fluidPage(
                  choices = levels(ds$F004),
                  selected = levels(ds$F004))
              )
+            
            )
-        )
-     )
+          ),
+         
+         a(href="https://github.com/mwilkens1/covid_survey/",
+           img(style="position: absolute; right: 0px; bottom: 0px",
+               src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+               align="right",
+               height=30))
+      )
+   )
 )
 
 
